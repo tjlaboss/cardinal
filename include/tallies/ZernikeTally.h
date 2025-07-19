24292a3e91de219bfb90e55ac5174e44ce856f4c
Nathan Glaser <glaserna@oregonstate.edu>
2025-07-19 13:57:15 -0500

    Header for zernike tally

---
+++ include/tallies/ZernikeTally.h
@@ -0,0 +1,44 @@
+/********************************************************************/
+/*                  SOFTWARE COPYRIGHT NOTIFICATION                 */
+/*                             Cardinal                             */
+/*                                                                  */
+/*                  (c) 2021 UChicago Argonne, LLC                  */
+/*                        ALL RIGHTS RESERVED                       */
+/*                                                                  */
+/*                 Prepared by UChicago Argonne, LLC                */
+/*               Under Contract No. DE-AC02-06CH11357               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*                With the U. S. Department of Energy               */
+/*                                                                  */
+/*                 See LICENSE for full restrictions                */
+/********************************************************************/
+
+#pragma once
+
+#include "FETallyBase.h"
+#include "openmc/tallies/filter_zernike.h"
+
+class ZernikeTally : public FETallyBase
+{
+  public:
+
+    static InputParameters validParams();
+
+    ZernikeTally(const InputParameters & parameters);
+
+    virtual std::pair<unsigned int, std::vector<openmc::Filter *>> spatialFilter() override;
+
+  protected:
+
+    virtual int getNumBins() override;
+
+    virtual int getNumSptlFilters() override {return 2;}
+
+    std::vector<unsigned> _orders;
+    const Point & _centroid;
+    const Real & _z_min;
+    const Real & _z_max;
+};
